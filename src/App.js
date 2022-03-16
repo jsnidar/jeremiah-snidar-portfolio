37338d545c9df8c1a003439c22b67524ea4a6404
Jeremiah <jsnidar@gmail.com>
2022-03-15 21:56:11 -0500

    Update font weight

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
         <Row className="pb-4">
           <Col lg={9} md={8} sm={7}>
             <h1 className="pt-4 mt-4 fs-1">Jeremiah Snidar</h1>
-            <h2 className="pt-3">Full Stack Software Engineer</h2>
-            <h3 className="pt-3">Javascript | React | Redux | Ruby | Rails</h3>
+            <h2 className="pt-3 fw-normal">Full Stack Software Engineer</h2>
+            <h3 className="pt-3 fw-light">Javascript | React | Redux | Ruby | Rails</h3>
           </Col>
           <Col className="pt-4 pe-2" lg={3} md={4} sm={5}>
             <Image
