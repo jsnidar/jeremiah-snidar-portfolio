c68adb25d2f53cff92775d27f23ce9c180c23a90
Jeremiah <jsnidar@gmail.com>
2022-03-16 10:08:38 -0500

    Set character limit

--- src/blogs/BlogCard.js
+++ src/blogs/BlogCard.js
@@ -22,7 +22,7 @@ const BlogCard = ({ post, profile }) => {
             >{post.title}</NavLink>
           </Card.Title>
           <Card.Text className="fs-5">
-            {`${ToText(post.description.slice(0, 1000))}...`}
+            {`${ToText(post.description.slice(0, 500))}...`}
           </Card.Text>
         </Card.Body>
       </Card>
