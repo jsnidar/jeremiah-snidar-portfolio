3fcf21a0ff17bfdbe1ec731dfdd0626e83f17f74
Jeremiah <jsnidar@gmail.com>
2022-03-29 13:57:42 -0500

    Add alert for email response.

--- src/contact/ContactForm.js
+++ src/contact/ContactForm.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, Row, Col, Container, Button } from "react-bootstrap";
+import { Form, Row, Col, Container, Button, Alert } from "react-bootstrap";
 import emailjs from '@emailjs/browser';
 import{ init } from '@emailjs/browser';
 init('REACT_APP_EMAILJS_USER_ID');
@@ -8,7 +8,9 @@ init('REACT_APP_EMAILJS_USER_ID');
 const ContactForm = () => {
   
   const [ formData, setFormData ] = useState({email: '', firstName: '', lastName: '', subject: '', message: ''});
-  console.log(formData)
+  const [ showAlert, setShowAlert ] = useState(false)
+  const [ alertContent, setAlertContent ] = useState({heading: '', message: ''})
+
   const handleChange = (e) => setFormData({...formData, [e.target.id]: e.target.value}) 
   
   const sendEmail = (e) => {
@@ -16,82 +18,102 @@ const ContactForm = () => {
 
     emailjs.send(process.env.REACT_APP_EMAILJS_SERVICE_ID, process.env.REACT_APP_EMAILJS_TEMPLATE_ID, formData, process.env.REACT_APP_EMAILJS_USER_ID)
       .then((result) => {
-          console.log(result.text);
+          setFormData({email: '', firstName: '', lastName: '', subject: '', message: ''})
+          setAlertContent({heading: "Thank you for contacting me.", message: "I will respond to your message as soon as I can."})
+          setShowAlert(true)
       }, (error) => {
-          console.log(error.text);
+        setAlertContent({heading: "Something went wrong.", message: "Please try again."})
+        setShowAlert(true)
       });
   };
-  
+
+  const alert = <>
+    <Alert variant="dark">
+      <Alert.Heading>{alertContent.heading}</Alert.Heading>
+      <p>
+        {alertContent.message}
+      </p>
+      <hr />
+      <div className="d-flex justify-content-end">
+        <Button onClick={() => setShowAlert(false)} variant="dark">
+          Return to Contact Form
+        </Button>
+      </div>
+    </Alert>
+  </>
+
+  const contactForm = <Form>
+    <Row>
+      <Col>
+        <Form.Group className="mb-3" controlId="firstName">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder=""
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </Form.Group>
+      </Col>
+      <Col>
+        <Form.Group className="mb-3" controlId="lastName">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder=""
+            onChange={handleChange}
+            value={formData.lastName}
+          />
+        </Form.Group>
+      </Col>
+    </Row>
+    <Row>
+      <Form.Group className="mb-3" controlId="email">
+        <Form.Label>Enter Email Address</Form.Label>
+        <Form.Control 
+          type="email" 
+          placeholder="name@example.com" 
+          onChange={handleChange}
+          value={formData.email}
+        />
+      </Form.Group>
+    </Row>
+    <Row>
+      <Form.Group className="mb-3" controlId="subject">
+        <Form.Label>Subject</Form.Label>
+        <Form.Control 
+          type="text" 
+          placeholder=""
+          onChange={handleChange}
+          value={formData.subject}
+        />
+      </Form.Group>
+    </Row>
+    <Row>
+      <Form.Group className="mb-3" controlId="message">
+        <Form.Label>Message</Form.Label>
+        <Form.Control 
+          as="textarea" 
+          placeholder="" 
+          onChange={handleChange}
+          value={formData.message}
+        />
+      </Form.Group>
+    </Row>
+    <Row>
+      <Button 
+        variant="yellow" 
+        type="submit"
+        onClick={e => sendEmail(e)}
+      >
+        Send
+      </Button>
+    </Row>
+  </Form>
+
   return (
     <Container>
-      <Form>
-        <Row>
-          <Col>
-            <Form.Group className="mb-3" controlId="firstName">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control 
-                type="text" 
-                placeholder=""
-                value={formData.firstName}
-                onChange={handleChange}
-              />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group className="mb-3" controlId="lastName">
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control 
-                type="text" 
-                placeholder=""
-                onChange={handleChange}
-                value={formData.lastName}
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          <Form.Group className="mb-3" controlId="email">
-            <Form.Label>Enter Email Address</Form.Label>
-            <Form.Control 
-              type="email" 
-              placeholder="name@example.com" 
-              onChange={handleChange}
-              value={formData.email}
-            />
-          </Form.Group>
-        </Row>
-        <Row>
-          <Form.Group className="mb-3" controlId="subject">
-            <Form.Label>Subject</Form.Label>
-            <Form.Control 
-              type="text" 
-              placeholder=""
-              onChange={handleChange}
-              value={formData.subject}
-            />
-          </Form.Group>
-        </Row>
-        <Row>
-          <Form.Group className="mb-3" controlId="message">
-            <Form.Label>Message</Form.Label>
-            <Form.Control 
-              as="textarea" 
-              placeholder="" 
-              onChange={handleChange}
-              value={formData.message}
-            />
-          </Form.Group>
-        </Row>
-        <Row>
-          <Button 
-            variant="yellow" 
-            type="submit"
-            onClick={e => sendEmail(e)}
-          >
-            Send
-          </Button>
-        </Row>
-      </Form>
+      {showAlert ? alert : contactForm}
     </Container>
   )
 }
