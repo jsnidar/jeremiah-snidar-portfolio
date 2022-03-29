6f77c867c81e61cb7cf19cf3af8d47ca5a868c97
Jeremiah <jsnidar@gmail.com>
2022-03-29 14:24:08 -0500

    Create compoenent for contact form alerts

---
+++ src/contact/ContactAlert.js
@@ -0,0 +1,18 @@
+
+
+const ContactAlert = ({alertContent, setShowAlert}) => {
+
+  return (<Alert variant="dark">
+      <Alert.Heading>{alertContent.heading}</Alert.Heading>
+      <p>
+        {alertContent.message}
+      </p>
+      <hr />
+      <div className="d-flex justify-content-end">
+        <Button onClick={() => setShowAlert(false)} variant="dark">
+          Return to Contact Form
+        </Button>
+      </div>
+    </Alert>)
+
+}
