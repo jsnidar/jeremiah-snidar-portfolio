6b9700786ff3957c480794ce7230a2159e8ab5bf
Jeremiah <jsnidar@gmail.com>
2024-10-03 21:15:17 -0500

    Standardize card heights

--- src/blogs/BlogCard.js
+++ src/blogs/BlogCard.js
@@ -3,26 +3,27 @@ import { Card, NavLink, Col } from "react-bootstrap"
 
 const BlogCard = ({ post }) => {
 
-  console.log(post)
-
   const ToText = (node) => {
     let tag = document.createElement('div')
     tag.innerHTML = node;
     node = tag.innerText;
     return node
   }
+
+  const description = ToText(post.description);
+
   return(
-    <Col sm={12} lg={6} xl={4} className='g-4'>
-      <Card className="bg-light border-3">
+    <Col sm={12} lg={6} className='g-4'>
+      <Card className="bg-light border-3 d-flex blog-card">
         <Card.Img variant="top" src={post.thumbnail} />
-        <Card.Body>
+        <Card.Body className="card-body">
           <Card.Title className="fs-4">
             <NavLink 
               onClick={() => window.open(`${post.link}`)}
             >{post.title}</NavLink>
           </Card.Title>
-          <Card.Text className="fs-5">
-            {`${ToText(post.description.slice(0, 400))}...`}
+          <Card.Text className="fs-5 description">
+            {description.length > 400 ? description.substring(0, 600) + '...' : description}
           </Card.Text>
         </Card.Body>
       </Card>
